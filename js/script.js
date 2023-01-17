b83589d9b59476fd778a7a0f62135c9942a52641
Caio Souza <caio.alberto@live.com>
2023-01-17 09:05:19 -0300

    updating code

--- js/script.js
+++ js/script.js
@@ -1,2 +1,28 @@
-let path = document.querySelector('#mux path');
-path.getTotalLength();
+// verificar a data de nascimento
+const birthDate = document.querySelector("#birth-date");
+
+birthDate.addEventListener("blur", (event) => {
+	checkBirthDate(event.target);
+});
+
+function checkBirthDate(input) {
+	const dateReceived = new Date(input.value);
+	let message = "";
+
+	if (!above18(dateReceived)) {
+		message = "Você deve ser maior que 18 anos para se cadastrar.";
+	}
+
+	input.setCustomValidity(message);
+}
+
+function above18(date) {
+	const currentDate = new Date();
+	const dateMore18 = new Date(
+		date.getUTCFullYear() + 18,
+		date.getUTCMonth(),
+		date.getUTCDate()
+	);
+
+	return dateMore18 <= currentDate;
+}
